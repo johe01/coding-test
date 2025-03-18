5bd3bc110a9f43b481bc4ddecca73c10d6ffb762
jhe05 <jhe0598@gmail.com>
2025-03-18 23:53:34 +0900

    Day5

---
+++ EntryLevelJava/src/Day5/PrintAge.java
@@ -0,0 +1,10 @@
+package Day5;
+
+public class PrintAge {
+    class Solution {
+        public int solution(int age) {
+            int answer = 2022 - age + 1;
+            return answer;
+        }
+    }
+}
